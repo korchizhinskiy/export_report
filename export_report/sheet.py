9c559ef64e3ba6d853470e60c4fb478f2c3bb25c
korchizhinskiy <korchizhinskiy@gmail.com>
2023-08-13 16:50:52 +0700

    feat: add component for sheet in report

---
+++ export_report/sheet.py
@@ -0,0 +1,18 @@
+from pandas import DataFrame
+
+from export_report.sheet_component import SheetComponent
+
+
+class ReportSheet:
+    """
+    Sheet class for use in report builder.
+    """
+
+    sheet_name: str
+    components: list[DataFrame] = []
+
+    def __init__(self, sheet_name: str) -> None:
+        self.sheet_name = sheet_name
+
+    def add_component(self, component: SheetComponent) -> None:
+        self.components.append(component.export_into_dataframe())
